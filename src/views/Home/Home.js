c637932e3e6ee4344296a6820646bff5b9c1faaf
Thai Nguyen <discoverylight@yahoo.com>
2019-07-17 09:09:37 -0700

    scaffolded the project

---
+++ src/views/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Home.scss";
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <h1 className="has-text-primary">This is Home</h1>
+    </div>
+  );
+};
+
+export default Home;
